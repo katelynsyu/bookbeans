050f4b19f1c9b20848d7a59c900e5019896d005a
Jody Lin <jody.m.lin@gmail.com>
2022-04-17 01:23:08 -0700

    remove extra safearea view (#24)

--- book-trading/src/screens/ProfileScreen/ProfileScreen.js
+++ book-trading/src/screens/ProfileScreen/ProfileScreen.js
@@ -1,26 +1,22 @@
 import { React, useState, useEffect } from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
 import ScreenContainer from '../../components/ScreenContainer.js';
 import ScreenTitle from '../../components/ScreenTitle.js';
-import { firebase } from "../../firebase/config";
 
 export default function ProfileScreen({ navigation, userData, ...props }) {
     return (
-      <SafeAreaView>
-        <ScreenContainer>
-          <ScreenTitle>Profile</ScreenTitle>
-          <View style={styles.imageContainer}>
-            <Image style={styles.profilePicture} source={require("../../../assets/yellow-bean.png")}/>
-          </View>
-          <View style={styles.textContainer}>
-            <Text style={styles.text}>Username</Text>
-            <Text style={styles.userText}>{userData.username}</Text>
-            <Text style={styles.text}>Email</Text>
-            <Text style={styles.userText}>{userData.email}</Text>
-          </View>
-        </ScreenContainer>
-      </SafeAreaView>
+      <ScreenContainer>
+        <ScreenTitle>Profile</ScreenTitle>
+        <View style={styles.imageContainer}>
+          <Image style={styles.profilePicture} source={require("../../../assets/yellow-bean.png")}/>
+        </View>
+        <View style={styles.textContainer}>
+          <Text style={styles.text}>Username</Text>
+          <Text style={styles.userText}>{userData.username}</Text>
+          <Text style={styles.text}>Email</Text>
+          <Text style={styles.userText}>{userData.email}</Text>
+        </View>
+      </ScreenContainer>
     );
 }
 
